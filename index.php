d9bbde1353f2491e3d701ac530122e654a1f5161
davidc2p <dadomingues@gmail.com>
2015-08-25 17:44:22 +0100

    Finishing insert method

--- index.php
+++ index.php
@@ -32,8 +32,39 @@
 	
 	$table1 = new webrickco\model\CRUD($model, 'cdo_house_pricing');
         $table1->id_house = 3;
-        $table->id_user = 'ddomingues';
+        $table1->id_user = 'ddomingues';
         $table1->id_season = 9;
+        $table1->dt_init = '2015-01-01';
+        $table1->dt_end = '2015-09-01';
+        
+//Array ( [Field] => dt_init [Type] => date [Null] => NO [Key] => [Default] => [Extra] => [Value] => ) 
+//
+//Array ( [Field] => dt_end [Type] => date [Null] => NO [Key] => [Default] => [Extra] => [Value] => ) 
+//
+//Array ( [Field] => monthprice [Type] => decimal(10,2) [Null] => YES [Key] => [Default] => [Extra] => [Value] => ) 
+//
+//Array ( [Field] => weekprice [Type] => decimal(10,2) [Null] => YES [Key] => [Default] => [Extra] => [Value] => ) 
+//
+//Array ( [Field] => nightweekprice [Type] => decimal(10,2) [Null] => YES [Key] => [Default] => [Extra] => [Value] => ) 
+//
+//Array ( [Field] => weekendprice [Type] => decimal(10,2) [Null] => YES [Key] => [Default] => [Extra] => [Value] => ) 
+//
+//Array ( [Field] => extranightweekendprice [Type] => decimal(10,2) [Null] => YES [Key] => [Default] => [Extra] => [Value] => ) 
+//
+//Array ( [Field] => extranightprice [Type] => decimal(10,2) [Null] => YES [Key] => [Default] => [Extra] => [Value] => ) 
+//
+//Array ( [Field] => specialeventprice [Type] => decimal(10,2) [Null] => YES [Key] => [Default] => [Extra] => [Value] => ) 
+//
+//Array ( [Field] => minimumunit [Type] => varchar(20) [Null] => NO [Key] => [Default] => [Extra] => [Value] => ) 
+//
+//Array ( [Field] => minimumvalue [Type] => int(11) [Null] => NO [Key] => [Default] => [Extra] => [Value] => ) 
+//
+//Array ( [Field] => dateupdate [Type] => timestamp [Null] => NO [Key] => [Default] => CURRENT_TIMESTAMP [Extra] => on update CURRENT_TIMESTAMP [Value] => ) 
+//
+//Array ( [Field] => dateinsert [Type] => timestamp [Null] => NO [Key] => [Default] => 0000-00-00 00:00:00 [Extra] => [Value] => ) 
+
+
+        
 	$table1->insert('cdo_house_pictures');
 
 ?>	
